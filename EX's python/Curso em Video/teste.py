a6a660fd38043a94bc49c7529cdd4f86818c15b5
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2023-09-25 08:50:03 -0300

    att

--- EX's python/Curso em Video/teste.py
+++ EX's python/Curso em Video/teste.py
@@ -1,11 +1,40 @@
-n1 = int(input("Digite o numero N1:"))
-n2 = int(input("Digite o numero N2:"))
-cont = 0
-aux = n1
 while True:
-    if aux - n2 > 0:
-        aux -= n2
-    else:
-        print(f"o Resto da divisão de {n1} por {n2} é {aux}")
+    n= total = float(input("Qual o valor que você deseja sacar?(Digite 0 para encerrar):"))
+    if n == 0:
         break
-
+    if n < 0.01:
+        print("Não se pode sacar valores tão pequenos")
+    ced = 100
+    tipo = "Notas"
+    contced = 0
+    while True:
+        if total >= ced:
+            while total >= ced:
+                total = total - ced
+                contced += 1
+            print(f"Você podera sacar {contced} {tipo} de {ced}R$")
+        if total == 0:
+            break
+        else:
+            if total < 100:
+                ced = 50
+                if total < 50:
+                    ced = 20
+                    if total <20:
+                        ced = 10
+                        if total < 10:
+                            ced = 4
+                            if total < 4:
+                                ced = 2
+                                if total < 2:
+                                    ced = 1
+                                    if total < 1:
+                                        tipo = "Moedas"
+                                        ced = 0.50
+                                        if total < 0.50:
+                                            ced = 0.10
+                                            if total < 0.10:
+                                                ced = 0.05
+                                                if total < 0.05:
+                                                    ced = 0.01
+        contced = 0
