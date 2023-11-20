caed053124d32825c322e98d5a97bdb374d6fa80
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2023-11-20 14:57:26 -0300

    att

--- EX's python/Curso em Video/teste.py
+++ EX's python/Curso em Video/teste.py
@@ -1,6 +1,11 @@
-def imprime_dicionario(dict):
-        print(dict)
+def somamatriz(matrix):
+    total = 0
+    for c in range(len(matrix)):
+        for i in matrix[c]:
+            total += sum(matrix[c])
+    return total
 
-dic = {"a":1,"b":2,"c":3}
-imprime_dicionario(dic)
 
+matrix = [[1,2,3,4,23,423],[12,321,3,1],[123,21,123,21,3],[12,3,12,312,3]]
+#print(somamatriz(matrix))
+print(somamatriz([[1,2],[2,4]]))
