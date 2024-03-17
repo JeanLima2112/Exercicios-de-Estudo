3634dcdb4fe1963f9e1863068b60437141d6a0f3
Jean Lima <132618223+JeanLima2112@users.noreply.github.com>
2024-03-17 20:26:13 -0300

    Update ex089.py

---
+++ Estudos/EX's python/ex089.py
@@ -0,0 +1,15 @@
+# get id of list in hexadecimal representation
+a = [1, 2, 3, 4, 5]
+print(hex(id(a)))
+  
+# get id of a variable in hexadecimal representation
+a = 12
+print(hex(id(a)))
+  
+# get id of tuple in hexadecimal representation
+a = (1, 2, 3, 4, 5)
+print(hex(id(a)))
+  
+# get id of a dictionary in hexadecimal representation
+a = {'a': 1,'b' : 2}
+print(hex(id(a)))
